32d6304e181ef5ab322e5be468ae1c7a3a812cda
angansop <arthur.tchipnang.ngansop@cgi.com>
2017-12-14 17:14:48 +0100

    Properties of NJIKE SCHOOL

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -12,7 +12,7 @@ angular.module('app')
 
       // config
       $scope.app = {
-        name: 'ISCHOOL',
+        name: 'GSB NJIKE',
         version: '2.0.0',
         // for chart colors
         color: {
