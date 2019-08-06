b64ab623a32450dd2ac0ec056563b6f9ab340f3a
Tedongmo <wilfried.tedongmo@nanobnk.com>
2019-08-06 08:33:55 +0100

    Update

--- src/test/java/TsoftApplicationTests.java
+++ src/test/java/TsoftApplicationTests.java
@@ -1,22 +1,22 @@
-
-import com.tsoft.ischool.ISchool;
-import com.tsoft.ischool.service.template.TemplateService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = ISchool.class)
-public class TsoftApplicationTests {
-
-    @Autowired
-    TemplateService templateService;
-
-    @Test
-    public void renderTemplate() throws Exception {
-       templateService.processTemplateEngine("com.tsoft.ischool");
-    }
-
-}
+//
+//import com.tsoft.ischool.ISchool;
+//import com.tsoft.ischool.service.template.TemplateService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = ISchool.class)
+//public class TsoftApplicationTests {
+//
+//    @Autowired
+//    TemplateService templateService;
+//
+//    @Test
+//    public void renderTemplate() throws Exception {
+//       templateService.processTemplateEngine("com.tsoft.ischool");
+//    }
+//
+//}
